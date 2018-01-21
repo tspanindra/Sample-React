8c97e4b6630f2f8bd6f636be3ac7799e33cba719
panindra Tumkur Seetharamu <ptumkurseetharamu@godaddy.com>
2018-01-21 13:42:24 -0800

    made production ready and added proper configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 const config = {
   resolve: {
@@ -7,17 +8,41 @@ const config = {
       path.resolve('./node_modules')
     ]
   },
-  entry:  [ 'babel-polyfill', './lib/renderers/dom.js'],
+  entry:  {
+    vendor: [
+      'babel-polyfill',
+      'react',
+      'react-dom',
+      'prop-types',
+      'lodash.debounce',
+      'lodash.pickby'
+    ],
+    app: ['./lib/renderers/dom.js'],
+  },
 
   output: {
     path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
   module: {
     rules: [
-      { test: /\.(js)$/, exclude: /node_modules/, use: 'babel-loader' }
+      { 
+        test: /\.(js)$/, exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['react', 'env', 'stage-2']
+          }
+        }
+      }
     ]
-  }
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: "vendor",
+    })
+  ]
+
 };
 
 module.exports = config;
